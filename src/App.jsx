afbb608c4b165f554dd679e75ff0564372047b6e
sksadiya <ssadiya.shaikh02@gmail.com>
2025-02-20 17:57:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
